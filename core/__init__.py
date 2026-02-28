7908e3a764705583f2f0924d1d9fbdd3d383cbaf
fizakhan896 <fizamubbsher@gmail.com>
2026-03-01 01:46:14 +0500

    Fiza: filled __init__.py files

---
+++ core/__init__.py
@@ -0,0 +1,8 @@
+from core.contracts import DataSink, PipelineService
+from core.engine import TransformationEngine
+
+__all__ = [
+    'DataSink',
+    'PipelineService',
+    'TransformationEngine'
+]
---
+++ plugins/__init__.py
@@ -0,0 +1,10 @@
+from plugins.inputs import JSONReader, CSVReader, ExcelReader
+from plugins.outputs import ConsoleWriter, GraphicsChartWriter
+
+__all__ = [
+    'JSONReader',
+    'CSVReader',
+    'ExcelReader',
+    'ConsoleWriter',
+    'GraphicsChartWriter'
+]
